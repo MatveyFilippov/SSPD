9483e870beea2569bcb2076ce8edfd3d1b95c3c9
MatveyFilippov <matdufi2004@gmail.com>
2025-09-05 22:31:48 +0300

    upgrade IgnoreFile by new FilePath

--- sspd/misc_helpers/ignoring_file.py
+++ sspd/misc_helpers/ignoring_file.py
@@ -1,16 +1,7 @@
+from .paths import FilePath
 import os
-import re
-
-
-def split_filepath(filepath: str) -> list[str]:
-    parts = re.split(r"(/|\\)+", filepath)
-    result = []
-    for part in parts:
-        part = part.strip()
-        if not part or part.count("/") or part.count("\\"):
-            continue
-        result.append(part.strip())
-    return result
+import sys
+from typing import Iterator
 
 
 class IgnKeyChars:
@@ -21,6 +12,7 @@ class IgnKeyChars:
 
 DESCRIPTION_SSPD_IGN = f"""{IgnKeyChars.comment_line_startswith} List files and folders to ignore during SSH/SCP project delivery
 {IgnKeyChars.comment_line_startswith} The system will only work with files in the local project folder specified in config
+{IgnKeyChars.comment_line_startswith} Always use forward slashes '/' for paths regardless of OS
 {IgnKeyChars.comment_line_startswith} Supported patterns:
 {IgnKeyChars.comment_line_startswith} - Lines starting with '{IgnKeyChars.comment_line_startswith}' are comments and will be ignored
 {IgnKeyChars.comment_line_startswith} - Lines ending with '{IgnKeyChars.folder_endswith}' indicate folders - all files within will be ignored
@@ -49,7 +41,7 @@ def init_default_ignore_file(filepath: str):
         return
     with open(filepath, "a+", encoding="UTF-8") as file:
         file.write("\n\n" + DEFAULT_SSPD_IGN_CONTENT)
-    os.abort()
+    sys.exit(0)
 
 
 class IgnoreFile:
@@ -59,52 +51,21 @@ class IgnoreFile:
             ignore_filepath += ".ign"
         if not os.path.exists(ignore_filepath):
             init_default_ignore_file(ignore_filepath)
-        self.IGNORE_FILEPATH = ignore_filepath
+        self._IGNORE_FILEPATH = ignore_filepath
 
         project_path = project_path.strip()
         if not project_path.endswith(os.path.sep):
             project_path += os.path.sep
         if not (os.path.exists(project_path) and os.path.isdir(project_path)):
             raise FileNotFoundError(f"Can't find project dir '{project_path}'")
-        self.PROJECT_DIR_PATH = project_path
+        self._PROJECT_DIR_PATH = project_path
 
-        self.__files2ignore = None
-        self.__absolute_filepaths2ignore = None
+        self.__files2ignore: set[FilePath] = set()
         self.__markers: set[str] = set()
-        self.__split_paths: list[list[str]] = []
 
-    @property
-    def files2ignore(self) -> set[str]:
-        if not self.__files2ignore:
-            self.update_files2ignore()
-        return self.__files2ignore.copy()
-
-    @property
-    def absolute_filepaths2ignore(self) -> set[str]:
-        if not self.__absolute_filepaths2ignore:
-            self.update_absolute_filepaths2ignore()
-        return self.__absolute_filepaths2ignore.copy()
-
-    def update_files2ignore(self):
-        self.__files2ignore = set()
-        self.__markers = set()
-        self.__split_paths = []
-        for line in self.__iter_ign_file_lines(skip_comment_lines=True):
-            if line.startswith(IgnKeyChars.filepath_startswith):
-                if line.endswith(IgnKeyChars.folder_endswith):
-                    self.__new_folderpath(line)
-                else:
-                    self.__new_filepath(line)
-            else:
-                while line.endswith("/") or line.endswith("\\"):
-                    line = line.removesuffix("/").removesuffix("\\")
-                self.__markers.add(line)
-        self.__update_split_paths_using_markers()
-        self.__set_filepaths_from_split_paths()
-
-    def __iter_ign_file_lines(self, skip_comment_lines=False) -> str:
-        with open(self.IGNORE_FILEPATH, "r") as file:
-            for line in file.readlines():
+    def __iter_ign_file_lines(self, skip_comment_lines=True) -> Iterator[str]:
+        with open(self._IGNORE_FILEPATH, "r") as file:
+            for line in file:
                 line = line.strip()
                 if skip_comment_lines and line.startswith(IgnKeyChars.comment_line_startswith):
                     continue
@@ -112,32 +73,45 @@ class IgnoreFile:
                     continue
                 yield line
 
-    def __update_split_paths_using_markers(self):
-        for root, dirs, files in os.walk(self.PROJECT_DIR_PATH):
-            for file in files:
-                filepath = split_filepath(
-                    os.path.join(root, file).replace(self.PROJECT_DIR_PATH, "")
-                )
-                for marker in self.__markers:
-                    if marker in filepath:
-                        self.__split_paths.append(filepath)
-
     def __new_filepath(self, filepath: str):
-        self.__split_paths.append(split_filepath(filepath))
+        self.__files2ignore.add(FilePath.from_filepath(filepath=filepath, project_folderpath=self._PROJECT_DIR_PATH))
 
     def __new_folderpath(self, folderpath: str):
         while folderpath.startswith("/"):
             folderpath = folderpath.removeprefix("/")
-        for root, dirs, files in os.walk(os.path.join(self.PROJECT_DIR_PATH, folderpath)):
+        if not folderpath.startswith(self._PROJECT_DIR_PATH):
+            folderpath = self._PROJECT_DIR_PATH + folderpath
+        for root, dirs, files in os.walk(folderpath):
             for file in files:
-                filepath = os.path.join(root, file).replace(self.PROJECT_DIR_PATH, "")
-                self.__new_filepath(filepath)
+                self.__new_filepath(os.path.join(root, file))
 
-    def __set_filepaths_from_split_paths(self):
-        for split_path in self.__split_paths:
-            self.__files2ignore.add(os.path.sep.join(split_path))
+    def __update_split_paths_using_markers(self):
+        for root, dirs, files in os.walk(self._PROJECT_DIR_PATH):
+            for file in files:
+                filepath = os.path.join(root, file)
+                for marker in self.__markers:
+                    if marker in filepath:
+                        self.__new_filepath(filepath)
 
-    def update_absolute_filepaths2ignore(self):
-        self.__absolute_filepaths2ignore.add(
-            os.path.join(self.PROJECT_DIR_PATH, filename) for filename in self.files2ignore
-        )
+    def update_files2ignore(self):
+        self.__files2ignore = set()
+        self.__markers = set()
+
+        for line in self.__iter_ign_file_lines():
+            if line.startswith(IgnKeyChars.filepath_startswith):
+                if line.endswith(IgnKeyChars.folder_endswith):
+                    self.__new_folderpath(line)
+                else:
+                    self.__new_filepath(line)
+            else:
+                while line.strip().endswith("/"):
+                    line = line.removesuffix("/")
+                self.__markers.add(line)
+
+        self.__update_split_paths_using_markers()
+
+    @property
+    def files2ignore(self) -> set[FilePath]:
+        if not self.__files2ignore:
+            self.update_files2ignore()
+        return self.__files2ignore.copy()
