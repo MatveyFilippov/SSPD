5a93b19873c342cc65239168032ca1a3af211550
MatveyFilippov <matdufi2004@gmail.com>
2025-09-05 22:29:30 +0300

    create module for Input/Output

---
+++ sspd/misc_helpers/io.py
@@ -0,0 +1,12 @@
+# TODO: here is a lot of print --- make it as param echo=True & add log_echo=False
+
+def print_info(text: str):
+    print("Info:", text)
+
+
+def print_request(text: str):
+    print("Local:", text)
+
+
+def print_response(text: str):
+    print("Remote:", text)
