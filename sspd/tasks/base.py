94769898ec5c03efa2b5969d306733c2e7328349
MatveyFilippov <matdufi2004@gmail.com>
2026-04-20 16:57:58 +0300

    refactoring: rename tasks

--- sspd/tasks/base.py
+++ sspd/tasks/base.py
@@ -7,8 +7,8 @@ import os
 REQUIREMENTS_FILE = FilePath("requirements.txt")
 
 
-def execute_remote_command(command: str, raise_on_error: bool = True,
-                           print_request: bool = True, print_response: bool = True) -> tuple[int, str]:
+def execute_command_in_remote_machine(command: str, raise_on_error: bool = True,
+                                      print_request: bool = True, print_response: bool = True) -> tuple[int, str]:
     if print_request:
         io.print_request(command)
 
@@ -34,7 +34,7 @@ def execute_remote_command(command: str, raise_on_error: bool = True,
     return 0, response
 
 
-def download_file_from_remote_server(remote_filepath: str, local_filepath: str):
+def download_file_from_remote_machine(remote_filepath: str, local_filepath: str):
     io.print_info(f"Downloading '{remote_filepath}' to '{local_filepath}'")
     try:
         with open(local_filepath, "wb") as file:
@@ -47,27 +47,27 @@ def download_file_from_remote_server(remote_filepath: str, local_filepath: str):
         raise exceptions.SSPDUnhandleableException(f"Can't write '{local_filepath}' in local machine")
 
 
-def download_folder_from_remote_server(remote_folderpath: str, local_folderpath: str):
+def download_folder_from_remote_machine(remote_folderpath: str, local_folderpath: str):
     os.makedirs(local_folderpath, exist_ok=True)
     for item in base.SFTP_REMOTE_MACHINE.listdir(remote_folderpath):
         remote_path = os.path.join(remote_folderpath, item)
         local_path = os.path.join(local_folderpath, item)
         if checker.is_remote_dir(remote_path):
-            download_folder_from_remote_server(remote_path, local_path)
+            download_folder_from_remote_machine(remote_path, local_path)
         else:
-            download_file_from_remote_server(remote_path, local_path)
+            download_file_from_remote_machine(remote_path, local_path)
 
 
-def create_folder_in_remote_server(remote_folderpath: str):
+def create_folder_in_remote_machine(remote_folderpath: str):
     if not checker.is_remote_dir(remote_folderpath):
         # base.SFTP_REMOTE_MACHINE.mkdir(remote_folderpath)
         io.print_info(f"Creating remote folder '{remote_folderpath}'")
-        execute_remote_command(f"mkdir -p {remote_folderpath}")
+        execute_command_in_remote_machine(f"mkdir -p {remote_folderpath}")
         io.print_info("Success")
 
 
-def send_file_to_remote_server(local_filepath: str, remote_filepath: str):
-    create_folder_in_remote_server(os.path.dirname(remote_filepath))
+def send_file_to_remote_machine(local_filepath: str, remote_filepath: str):
+    create_folder_in_remote_machine(os.path.dirname(remote_filepath))
     try:
         io.print_info(f"Sending '{local_filepath}' to '{remote_filepath}'")
         with open(local_filepath, "rb") as file:
@@ -77,18 +77,18 @@ def send_file_to_remote_server(local_filepath: str, remote_filepath: str):
         raise exceptions.SSPDUnhandleableException(f"No such file '{local_filepath}' in local machine")
 
 
-def send_folder_to_remote_server(local_folderpath: str, remote_folderpath: str):
-    create_folder_in_remote_server(remote_folderpath)
+def send_folder_to_remote_machine(local_folderpath: str, remote_folderpath: str):
+    create_folder_in_remote_machine(remote_folderpath)
     for item in os.listdir(local_folderpath):
         local_path = os.path.join(local_folderpath, item)
         remote_path = os.path.join(remote_folderpath, item)
         if os.path.isdir(local_path):
-            send_folder_to_remote_server(local_path, remote_path)
+            send_folder_to_remote_machine(local_path, remote_path)
         else:
-            send_file_to_remote_server(local_path, remote_path)
+            send_file_to_remote_machine(local_path, remote_path)
 
 
-def delete_file_in_remote_server(remote_filepath: str):
+def delete_file_in_remote_machine(remote_filepath: str):
     io.print_info(f"Deleting remote file '{remote_filepath}'")
     if not checker.is_remote_file(remote_filepath):
         raise exceptions.SSPDUnhandleableException(f"No such file '{remote_filepath}' in remote machine")
@@ -96,37 +96,45 @@ def delete_file_in_remote_server(remote_filepath: str):
     io.print_info("Success")
 
 
-def delete_folder_in_remote_server(remote_folderpath: str):
+def delete_folder_in_remote_machine(remote_folderpath: str):
     for item in base.SFTP_REMOTE_MACHINE.listdir(remote_folderpath):
         if checker.is_remote_dir(item):
-            delete_folder_in_remote_server(remote_folderpath)
-        delete_file_in_remote_server(item)
+            delete_folder_in_remote_machine(remote_folderpath)
+        delete_file_in_remote_machine(item)
     io.print_info(f"Deleting remote folder '{remote_folderpath}'")
     base.SFTP_REMOTE_MACHINE.rmdir(remote_folderpath)
     io.print_info("Success")
 
 
-def stop_running_remote_code(raise_on_error: bool = True) -> tuple[int, str]:
-    io.print_info("Stop running remote py code")
-    return execute_remote_command(f"sudo systemctl stop {base.REMOTE_SERVICE_FILENAME}", raise_on_error=raise_on_error)
+def stop_running_remote_service(raise_on_error: bool = True) -> tuple[int, str]:
+    io.print_info("Stop running remote service")
+    return execute_command_in_remote_machine(
+        f"sudo systemctl stop {base.REMOTE_SERVICE_FILENAME}", raise_on_error=raise_on_error,
+    )
 
 
-def start_running_remote_code(raise_on_error: bool = True) -> tuple[int, str]:
-    io.print_info("Start running remote py code")
-    return execute_remote_command(f"sudo systemctl start {base.REMOTE_SERVICE_FILENAME}", raise_on_error=raise_on_error)
+def start_running_remote_service(raise_on_error: bool = True) -> tuple[int, str]:
+    io.print_info("Start running remote service")
+    return execute_command_in_remote_machine(
+        f"sudo systemctl start {base.REMOTE_SERVICE_FILENAME}", raise_on_error=raise_on_error,
+    )
 
 
-def restart_running_remote_code(raise_on_error: bool = True) -> tuple[int, str]:
+def restart_running_remote_service(raise_on_error: bool = True) -> tuple[int, str]:
     io.print_info("Reload daemons")
-    execute_remote_command(f"sudo systemctl daemon-reload")
-    io.print_info("Restart running remote py code")
-    return execute_remote_command(f"sudo systemctl restart {base.REMOTE_SERVICE_FILENAME}", raise_on_error=raise_on_error)
+    execute_command_in_remote_machine(f"sudo systemctl daemon-reload")
+    io.print_info("Restart running remote service")
+    return execute_command_in_remote_machine(
+        f"sudo systemctl restart {base.REMOTE_SERVICE_FILENAME}", raise_on_error=raise_on_error,
+    )
 
 
-def run_reinstalling_remote_requirements(raise_on_error: bool = True) -> tuple[int, str]:
+def run_installing_requirements_in_remote_machine(raise_on_error: bool = True) -> tuple[int, str]:
     io.print_info(f"Try to reinstall requirements in remote '{base.CORE_VENV_DIR_NAME}'")
-    return execute_remote_command((
-        f"{base.REMOTE_PROJECT_DIR_PATH}/{base.CORE_VENV_DIR_NAME}/bin/pip"
-        " install -r "
-        f"{REQUIREMENTS_FILE.to_absolute(base.REMOTE_PROJECT_DIR_PATH)}"
-    ), raise_on_error=raise_on_error)
+    return execute_command_in_remote_machine(
+        (
+            f"{base.REMOTE_PROJECT_DIR_PATH}/{base.CORE_VENV_DIR_NAME}/bin/pip"
+            " install -r "
+            f"{REQUIREMENTS_FILE.to_absolute(base.REMOTE_PROJECT_DIR_PATH)}"
+        ), raise_on_error=raise_on_error,
+    )
--- sspd/tasks/mastered.py
+++ sspd/tasks/mastered.py
@@ -5,32 +5,32 @@ from ..utils import io
 from ..utils.paths import FilePath
 
 
-def download_log_file():
+def download_log_file_from_remote_machine():
     if checker.is_download_log_file_available():
-        base.download_file_from_remote_server(
+        base.download_file_from_remote_machine(
             remote_filepath=sspd_properties.REMOTE_LOG_FILE_PATH,
             local_filepath=sspd_properties.LOCAL_LOG_FILE_PATH_TO_DOWNLOAD_IN,
         )
 
 
-def send_files_from_project_dir(files: set[FilePath]):
+def send_files_to_remote_project(files: set[FilePath]):
     io.print_info("Start sending files from local to remote project dir")
     for file in files:
         local_filepath = file.to_absolute(sspd_properties.LOCAL_PROJECT_DIR_PATH)
         remote_filepath = file.to_absolute(sspd_properties.REMOTE_PROJECT_DIR_PATH)
-        base.send_file_to_remote_server(local_filepath, remote_filepath)
+        base.send_file_to_remote_machine(local_filepath, remote_filepath)
     io.print_info("All files are send to remote project dir")
 
 
-def delete_files_from_project_dir(files: set[FilePath]):
+def delete_files_in_remote_project_dir(files: set[FilePath]):
     io.print_info("Start deleting files in remote project dir")
     for file in files:
         remote_filepath = file.to_absolute(sspd_properties.REMOTE_PROJECT_DIR_PATH)
-        base.delete_file_in_remote_server(remote_filepath)
+        base.delete_file_in_remote_machine(remote_filepath)
     io.print_info("All files are delete in remote project dir")
 
 
-def update_remote_code(run_after_update: bool = True):
+def update_remote_project(run_after_update: bool = True):
     io.print_info("Start updating remote code")
 
     io.print_info("Look differences in local and remote files")
@@ -55,19 +55,19 @@ def update_remote_code(run_after_update: bool = True):
         io.print_info("Break process...")
         return
 
-    status, response = base.stop_running_remote_code(raise_on_error=False)
+    status, response = base.stop_running_remote_service(raise_on_error=False)
     if status == -1:
         io.print_info("While stop running was unexpected error, do you want to break process?")
         if io.input_bool("ENTER (to continue) / '{sign2break}' (to break): ", sign2break="Br"):
             raise exceptions.SSPDUnhandleableException(response)
 
-    send_files_from_project_dir(files2send)
-    delete_files_from_project_dir(files2delete)
+    send_files_to_remote_project(files2send)
+    delete_files_in_remote_project_dir(files2delete)
 
     if base.REQUIREMENTS_FILE in files2send:
-        base.run_reinstalling_remote_requirements()
+        base.run_installing_requirements_in_remote_machine()
 
     if run_after_update:
-        base.start_running_remote_code()
+        base.start_running_remote_service()
 
     io.print_info("All is done -> files in remote server are up to date!")
